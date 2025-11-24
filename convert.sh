400834c9d904cf196b7c3bf10c3d33850d4a3a83
Thomas Michaud <thomas.michaud@gmail.com>
2025-11-24 18:41:34 -0500

    .

--- convert.sh
+++ convert.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # Note: Requires libreoffice to be installed
+# Note: Requires pandoc to be installed (sudo apt install pandoc)
 
-libreoffice --headless --convert-to pdf  --outdir .  ./Resume.docx
-libreoffice --headless --convert-to html --outdir .  ./Resume.docx && mv Resume.html README.md
+libreoffice --headless --convert-to pdf --outdir .  ./Resume.docx
+pandoc Resume.docx -o README.md
